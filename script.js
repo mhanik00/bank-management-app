2588134c8700395dbe2b6f19d5f6ccb5043bcf53
mhanik00 <mhanik063@gmail.com>
2024-03-30 04:02:57 +0600

    bug fix

--- script.js
+++ script.js
@@ -8,20 +8,21 @@ function depositAdd() {
     const depositAm = document.getElementById('depositAmount');
     const depositNum = depositAm.value.trim();
 
+    // check validity of deposit
     if (depositNum === '' || depositNum === NaN || depositNum < 0) {
         alert('Please Enter A Valid Amount');
         return;
     }
 
-    const currentDeposit = document.getElementById('currentDeposit');
-
     // update deposit amount 
+    const currentDeposit = document.getElementById('currentDeposit');
     currentDeposit.innerText = depositNum;
 
-    const currentBalance = document.getElementById('balanceAmount');
-
     // update balance after adding the new deposit to it
+    const currentBalance = document.getElementById('balanceAmount');
     currentBalance.innerText = Number(currentBalance.innerText) + Number(depositNum);
+
+    // updated deposit amount back to empty for next use
     depositAm.value = '';
 }
 
@@ -30,41 +31,36 @@ function withdrawAdd() {
 
     // withdraw input value
     const withdrawAm = document.getElementById('withdrawAmount');
-
     const withdrawNum = withdrawAm.value.trim();
 
-    if (withdrawNum === '' || withdrawNum === NaN || withdrawNum < 0 ) {
+    //! place currentBalance here for fix in case of insufficient balance
+    const currentBalance = document.getElementById('balanceAmount');
 
-       
+    // check validity of withdraw
+    if (withdrawNum === '' || withdrawNum === NaN || withdrawNum < 0) {
         alert('Please Enter A Valid Amount');
         return;
     }
 
+    // updated withdraw amount
     const currentWithdraw = document.getElementById('withdraw');
-
-    // update withdraw amount
     currentWithdraw.innerText = withdrawNum;
 
-    
-    const currentBalance = document.getElementById('balanceAmount');
-
-    // update balance after adding the new withdraw to it
-    
-   
-    if (currentBalance.innerText <! withdrawNum || withdrawNum <= currentBalance.innerText ) {
-       
-        currentBalance.innerText = Number(currentBalance.innerText) - Number(withdrawNum);
-       
-    
-      
-    } else {
-        
-        currentWithdraw.innerText=0;
-       
+    //! check balance is sufficient or not for withdraw(if withdraw is greater than balance then show alert & do not update balance)
+    if (Number(withdrawNum) > Number(currentBalance.innerText)) {
 
         alert('Insufficient Balance');
+
+        // updated withdraw amount back to empty when insufficient balance
+        currentWithdraw.innerText = 0;
+        return;
     }
-    
+
+    //* updated balance if balance is sufficient for withdraw
+    else {
+        // update balance after adding the new withdraw to it
+        currentBalance.innerText = Number(currentBalance.innerText) - Number(withdrawNum);
+    }
     withdrawAm.value = '';
 }
 
